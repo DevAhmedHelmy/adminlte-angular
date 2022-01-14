4b3c7492bc7a1294902bc286e28ba48aa9da1620
Ahmed <helmyahmed248@gmail.com>
2022-01-14 21:16:45 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { NavbarComponent } from './components/layouts/navbar/navbar.component';
+import { SidebarComponent } from './components/layouts/sidebar/sidebar.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { BreadcrumbComponent } from './components/layouts/breadcrumb/breadcrumb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    HomeComponent,
+    BreadcrumbComponent
   ],
   imports: [
     BrowserModule
