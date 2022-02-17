a33e05ca4594c347d198f9b91fe1bc7280b03ca7
Rana Hisham Foad <rana@megatrust.co>
2022-02-17 12:13:19 +0200

    ahmed

--- src/app/dashboard/authentication/components/login/login.component.ts
+++ src/app/dashboard/authentication/components/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private authService: AuthenticationService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login() {
+    this.authService.login();
   }
-
 }
--- src/app/dashboard/secure/secure.module.ts
+++ src/app/dashboard/secure/secure.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SecureComponent } from './secure.component';
 import { RouterModule, Routes } from '@angular/router';
+import { BreadcrumbComponent } from './components/layouts/breadcrumb/breadcrumb.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { NavbarComponent } from './components/layouts/navbar/navbar.component';
+import { SidebarComponent } from './components/layouts/sidebar/sidebar.component';
+import { HomeComponent } from './components/pages/home/home.component';
 const routes: Routes = [
   {
     path: 'dashboard',
@@ -13,7 +18,14 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [SecureComponent],
+  declarations: [
+    SecureComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    HomeComponent,
+    BreadcrumbComponent,
+  ],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class SecureModule {}
